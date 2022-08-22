cfd88fa3010f324b2972a8820d9e7f84c74d4cfb
SEONMl <sm000117@naver.com>
2022-08-22 13:28:25 +0900

    feat: 프로젝트 생성

---
+++ src/test/java/com/haruhanjan/authentication/AuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haruhanjan.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
