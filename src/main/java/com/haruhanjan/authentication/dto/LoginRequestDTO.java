e26753a15d48912fd2373a303f7b1205a998dd56
SEONMl <sm000117@naver.com>
2022-08-26 16:18:26 +0900

    feat: #2 JWT 토큰 발급 후 쿠키 설정

--- src/main/java/com/haruhanjan/authentication/dto/LoginRequestDTO.java
+++ src/main/java/com/haruhanjan/authentication/dto/LoginRequestDTO.java
@@ -1,10 +1,16 @@
 package com.haruhanjan.authentication.dto;
 
+import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
 @Setter
+@Getter
 public class LoginRequestDTO {
-
     private String accountId;
     private String password;
+
+    public UsernamePasswordAuthenticationToken toAuthentication() {
+        return new UsernamePasswordAuthenticationToken(accountId,password);
+    }
 }
