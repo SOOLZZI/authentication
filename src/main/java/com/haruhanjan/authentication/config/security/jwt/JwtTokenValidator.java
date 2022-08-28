520aef30787942b8fe713dfb5ecb1b31bceeb6d9
Hyeonjun Park <phjppo0918@gmail.com>
2022-08-28 23:59:25 +0900

    feat: jwt token validator 구현

--- src/main/java/com/haruhanjan/authentication/config/security/jwt/JwtTokenValidator.java
+++ src/main/java/com/haruhanjan/authentication/config/security/jwt/JwtTokenValidator.java
@@ -1,10 +1,31 @@
 package com.haruhanjan.authentication.config.security.jwt;
 
+import com.haruhanjan.authentication.dto.UserAuthResponse;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Component
+@RequiredArgsConstructor
 public class JwtTokenValidator {
 
+    private final Key key;
 
-    /*
-    1. 토큰 유효성 확인
-    2. 토큰 안에 accountId, ROLE 확인
-     */
+    public UserAuthResponse getAuthentication(String accessToken) {
+        Claims claims = getTokenBodyClaims(accessToken);
+
+        return new UserAuthResponse(claims);
+    }
+
+    private Claims getTokenBodyClaims(String accessToken) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(accessToken)
+                .getBody();
+    }
+
 }
