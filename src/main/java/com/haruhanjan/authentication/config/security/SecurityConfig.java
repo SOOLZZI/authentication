ac4bbe0e5b32ac6e07e4b7ebc273171b15dfd0e9
Hyeonjun Park <phjppo0918@gmail.com>
2022-08-29 14:43:36 +0900

    delete: jwtFilter 삭제

--- src/main/java/com/haruhanjan/authentication/config/security/SecurityConfig.java
+++ src/main/java/com/haruhanjan/authentication/config/security/SecurityConfig.java
@@ -15,9 +15,6 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-
-    private final JwtFilter jwtFilter;
-
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
