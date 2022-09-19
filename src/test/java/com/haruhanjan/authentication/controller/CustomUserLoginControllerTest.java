751246cde9610e6e0103035431932287c140a7c1
SEONMl <sm000117@naver.com>
2022-09-19 21:49:50 +0900

    fix: test code

--- src/test/java/com/haruhanjan/authentication/controller/CustomUserLoginControllerTest.java
+++ src/test/java/com/haruhanjan/authentication/controller/CustomUserLoginControllerTest.java
@@ -1,18 +1,28 @@
 package com.haruhanjan.authentication.controller;
 
+import com.haruhanjan.authentication.service.AuthService;
+import com.haruhanjan.authentication.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-@ExtendWith({ SpringExtension.class})
+@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
 @WebMvcTest(CustomUserLoginController.class)
 @AutoConfigureRestDocs
 class CustomUserLoginControllerTest {
 
+    @MockBean
+    private UserService userService;
+    @MockBean
+    private AuthService authService;
+    @Autowired
+    private MockMvc mockMvc; // http 호출
     @Test
     void getAll() {
     }
